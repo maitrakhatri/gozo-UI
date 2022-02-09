c7fd6168a8d76d7772ad44a860502aa54ce80e31
Maitra Khatri <maitrakhatri@gmail.com>
2022-02-09 23:53:35 +0530

    restructured the entire documentation for better navigation and fast loading time

--- JS/slider.js
+++ JS/slider.js
@@ -5,4 +5,4 @@ const output = document.getElementById("slider-value");
 output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
-slider.addEventListener("input", () => output.innerText = slider.value );
+slider.addEventListener("input", () => output.innerHTML = slider.value);
